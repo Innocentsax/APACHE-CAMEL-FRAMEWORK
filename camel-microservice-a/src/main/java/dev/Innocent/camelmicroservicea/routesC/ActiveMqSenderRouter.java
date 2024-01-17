0064a6cfdfa93d3f4b3304fa7dffeeccd2ebfab5
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-17 16:18:26 +0100

    Camel Microservice a

---
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesC/ActiveMqSenderRouter.java
@@ -0,0 +1,17 @@
+package dev.Innocent.camelmicroservicea.routesC;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        //timer
+        from("timer:active-mq-timer?period=10000")
+                .transform().constant("My message for Active MQ")
+                .log("${body}")
+                .to("activemq:my-activemq-queue");
+        //queue
+    }
+}
