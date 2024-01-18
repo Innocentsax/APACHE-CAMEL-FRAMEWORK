0f31a78d8e2462ede0deb1ca61d58824558f27bb
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-18 14:41:46 +0100

    Receiving response from ActiveMq

--- camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
+++ camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
@@ -1,13 +1,23 @@
 package dev.Innocent.camelmicroserviceb.routes;
 
+import dev.Innocent.camelmicroserviceb.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
+    @Autowired
+    private MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
     @Override
     public void configure() throws Exception {
         from("activemq:my-activemq-queue")
+                .unmarshal().json(JsonLibrary.Jackson, CurrencyExchange.class)
                 .to("log:received-message-from-active-mq");
     }
+}
+@Component
+class MyCurrencyExchangeProcessor{
+
 }
