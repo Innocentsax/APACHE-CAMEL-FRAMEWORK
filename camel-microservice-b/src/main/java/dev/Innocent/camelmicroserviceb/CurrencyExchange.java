f93f2b43e66c59b765db4927d035026caa782250
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-18 14:41:56 +0100

    Receiving response from ActiveMq

---
+++ camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/CurrencyExchange.java
@@ -0,0 +1,62 @@
+package dev.Innocent.camelmicroserviceb;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyExchange{" +
+                "id=" + id +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", conversionMultiple=" + conversionMultiple +
+                '}';
+    }
+
+    public CurrencyExchange() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+}
