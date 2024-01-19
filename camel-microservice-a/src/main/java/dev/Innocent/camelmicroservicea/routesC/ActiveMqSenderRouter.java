dabe2bf7761ddf16cd61e9196cf228dc2cbf76d1
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-19 18:24:30 +0100

    Camel Microservice a

--- camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesC/ActiveMqSenderRouter.java
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesC/ActiveMqSenderRouter.java
@@ -1,9 +1,8 @@
 package dev.Innocent.camelmicroservicea.routesC;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
