3afaa198c5dd02cecf6f7322d6b6eff90b8c8454
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-19 18:24:47 +0100

    Camel Microservice a

---
+++ camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/routes/KafkaReceiverRouter.java
@@ -0,0 +1,13 @@
+package dev.Innocent.camelmicroserviceb.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaReceiverRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("kafka:myKafkaTopic")
+                .to("log:received-message-from-kafka");
+    }
+}
