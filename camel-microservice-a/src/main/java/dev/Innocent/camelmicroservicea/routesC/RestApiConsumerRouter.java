911b87d4242be9b4fd3da062a3b8a865940fe2bb
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-21 17:29:58 +0100

    Camel Microservice a

--- camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesC/RestApiConsumerRouter.java
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesC/RestApiConsumerRouter.java
@@ -1,9 +1,8 @@
 package dev.Innocent.camelmicroservicea.routesC;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class RestApiConsumerRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
