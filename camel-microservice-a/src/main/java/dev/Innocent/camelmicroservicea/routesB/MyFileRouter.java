bce726a9bcf4dea66da8cd88ab5c80f142640d95
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-21 17:29:54 +0100

    Camel Microservice a

--- camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesB/MyFileRouter.java
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesB/MyFileRouter.java
@@ -1,13 +1,23 @@
 package dev.Innocent.camelmicroservicea.routesB;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:files/input")
-                .log("${body}")
+                .routeId("Files-Input-Route")
+                .transform().body(String.class)
+                .choice()
+                    .when(simple("${file:ext} ends with 'xml'"))
+                        .log("XML FILE")
+                    .when(simple("${body} contains 'USD'"))
+                        .log("Not an XML File but Contains USD")
+                    .otherwise().log("Not an XML FILE")
+                .end()
+                .log("${messageHistory} ${headers.CamelFileAbsolute}")
                 .to("file:files/output");
     }
 }
