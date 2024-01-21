246d878b69270ba3d3b376bad78a51a41ac409c1
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-21 23:09:33 +0100

    Camel Microservice a

--- camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesB/MyFileRouter.java
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesB/MyFileRouter.java
@@ -1,10 +1,20 @@
 package dev.Innocent.camelmicroservicea.routesB;
 
+import org.apache.camel.Body;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class MyFileRouter extends RouteBuilder {
+    @Autowired
+    private DeciderBean deciderBean;
     @Override
     public void configure() throws Exception {
         from("file:files/input")
@@ -13,11 +23,12 @@ public class MyFileRouter extends RouteBuilder {
                 .choice()
                     .when(simple("${file:ext} == 'xml'"))
                         .log("XML FILE")
-                    .when(simple("${body} contains 'USD'"))
+//                    .when(simple("${body} contains 'USD'"))
+                .when(method(deciderBean))
                         .log("Not an XML File but Contains USD")
                     .otherwise().log("Not an XML FILE")
                 .end()
-                .log("${messageHistory} ${file:absolute.path}")
+                .to("direct://log-file-values")
                 .to("file:files/output");
 
         from("direct:log-file-values")
@@ -26,5 +37,14 @@ public class MyFileRouter extends RouteBuilder {
                 .log("${file:onlyname.noext} ${file:parent} ${file:path} ${file:absolute}")
                 .log("${file:size} ${file:modified}")
                 .log("${routeId} ${camelId} ${body}");
+    }
+}
+@Component
+class DeciderBean{
+    Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+    public boolean isThisConditionMet(@Body String body, @Headers Map<String, String> headers,
+                                      @ExchangeProperties Map<String, String> exchangeProperties){
+        logger.info("DeciderBean {} {} {}", body, headers, exchangeProperties);
+        return true;
     }
 }
