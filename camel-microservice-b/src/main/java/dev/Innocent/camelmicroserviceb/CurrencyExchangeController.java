8f92a1f648dbacc1779e24bece95f4a79c521da5
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-21 16:53:47 +0100

    camel microservice b

--- camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/CurrencyExchangeController.java
+++ camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/CurrencyExchangeController.java
@@ -1,4 +1,15 @@
 package dev.Innocent.camelmicroserviceb;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CurrencyExchangeController {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange findConversionValue(@PathVariable String from, @PathVariable String to){
+        return new CurrencyExchange(10001L,"USD","INR", BigDecimal.TEN);
+    }
 }
