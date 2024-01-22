c6a5ba0ec342411615978f792de11b32534c822c
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-22 17:07:45 +0100

    Camel Microservice a

---
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/EpiPatternsRouter.java
@@ -0,0 +1,12 @@
+package dev.Innocent.camelmicroservicea;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EpiPatternsRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
