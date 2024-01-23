931f2f0db5c76342a0f35d2b60a2668b55954799
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-23 10:51:25 +0100

    Camel Microservice a

--- camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/EpiPatternsRouter.java
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/EpiPatternsRouter.java
@@ -1,25 +1,45 @@
 package dev.Innocent.camelmicroservicea;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class EpiPatternsRouter extends RouteBuilder {
+    @Autowired
+    SplitterComponent splitterComponent;
     @Override
     public void configure() throws Exception {
         // Pipeline
         // Content based Routing - choice
         // Multicast
-        from("timer:multicast?period=10000")
-                .multicast()
-                .to("log:something1", "log:somethings2");
+//        from("timer:multicast?period=10000")
+//                .multicast()
+//                .to("log:something1", "log:somethings2");
+//
+//        from("file:files/csv")
+//                .unmarshal().csv()
+//                .split(body())
+//                .to("activemq:split-queue");
+//
+//        from("activemq:split-queue")
+//                .to("log:received-message-from-active-mq");
 
+
+        // message, message2, Message3
         from("file:files/csv")
-                .unmarshal().csv()
-                .split(body())
+//                .unmarshal().csv()
+                .convertBodyTo(String.class)
+                .split(body(),",")
                 .to("activemq:split-queue");
+    }
+}
 
-        from("activemq:split-queue")
-                .to("log:received-message-from-active-mq");
+@Component
+class SplitterComponent{
+    public List<String> splitInput(String body){
+        return List.of("ABC", "DEF", "GHI");
     }
 }
