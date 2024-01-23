dbb21a23885f105c2f7fd6098fc095d924c2f6fe
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-23 18:23:48 +0100

    camel microservice b

--- camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
+++ camel-microservice-b/src/main/java/dev/Innocent/camelmicroserviceb/routes/ActiveMqReceiverRouter.java
@@ -2,12 +2,16 @@ package dev.Innocent.camelmicroserviceb.routes;
 
 import dev.Innocent.camelmicroserviceb.CurrencyExchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.crypto.CryptoDataFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Component
 public class ActiveMqReceiverRouter extends RouteBuilder {
@@ -27,9 +31,23 @@ public class ActiveMqReceiverRouter extends RouteBuilder {
 //                .unmarshal().jacksonXml(CurrencyExchange.class)
 //                .to("log:received-message-from-active-mq");
 
-        from("file:files/xml")
-                .log("${body}")
-                .to("activemq:my-activemq-xml-queue");
+//        from("file:files/xml")
+//                .log("${body}")
+//                .to("activemq:my-activemq-xml-queue");
+
+        from("activemq:split-queue")
+                .unmarshal(createEncryptor())
+                .to("log:received-message-from-active-mq");
+    }
+    private CryptoDataFormat createEncryptor() throws KeyStoreException, IOException, NoSuchAlgorithmException,
+            CertificateException, UnrecoverableKeyException {
+        KeyStore keyStore = KeyStore.getInstance("JCEKS");
+        ClassLoader classLoader = getClass().getClassLoader();
+        keyStore.load(classLoader.getResourceAsStream("myDesKey.jceks"), "someKeystorePassword".toCharArray());
+        Key sharedKey = keyStore.getKey("myDesKey", "someKeyPassword".toCharArray());
+
+        CryptoDataFormat sharedKeyCrypto = new CryptoDataFormat("DES", sharedKey);
+        return sharedKeyCrypto;
     }
 }
 @Component
