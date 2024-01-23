8205b246be9ce3cd04879c3507913a21d5d4dd3a
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-23 18:21:24 +0100

    Camel Microservice a

--- camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesC/ActiveMqSenderRouter.java
+++ camel-microservice-a/src/main/java/dev/Innocent/camelmicroservicea/routesC/ActiveMqSenderRouter.java
@@ -1,26 +1,43 @@
 package dev.Innocent.camelmicroservicea.routesC;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.crypto.CryptoDataFormat;
+
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 //@Component
 public class ActiveMqSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        //timer
-//        from("timer:active-mq-timer?period=10000")
-//                .transform().constant("My message for Active MQ")
-//                .log("${body}")
-//                .to("activemq:my-activemq-queue");
+//        timer
+        from("timer:active-mq-timer?period=10000")
+                .transform().constant("My message for Active MQ")
+                .log("${body}")
+                .unmarshal(createEncryptor())
+                .to("activemq:my-activemq-queue");
         //queue
 
 //        from("file:files/json")
 //                .log("${body}")
 //                .to("activemq:my-activemq-queue");
 
-        from("file:files/xml")
-                .log("${body}")
-                .to("activemq:my-activemq-xml-queue");
+//        from("file:files/xml")
+//                .log("${body}")
+//                .to("activemq:my-activemq-xml-queue");
 
 
+    }
+
+    private CryptoDataFormat createEncryptor() throws KeyStoreException, IOException, NoSuchAlgorithmException,
+            CertificateException, UnrecoverableKeyException {
+        KeyStore keyStore = KeyStore.getInstance("JCEKS");
+        ClassLoader classLoader = getClass().getClassLoader();
+        keyStore.load(classLoader.getResourceAsStream("myDesKey.jceks"), "someKeystorePassword".toCharArray());
+        Key sharedKey = keyStore.getKey("myDesKey", "someKeyPassword".toCharArray());
+
+        CryptoDataFormat sharedKeyCrypto = new CryptoDataFormat("DES", sharedKey);
+        return sharedKeyCrypto;
     }
 }
